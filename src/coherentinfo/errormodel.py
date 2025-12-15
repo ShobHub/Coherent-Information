9c6c744d2ff03998a8ce808cf88fe268756ab7c5
Alessandro Ciani <alessandrociani89@gmail.com>
2025-12-15 05:55:05 +0100

    Added simple Bernoulli error model and jax version

--- src/coherentinfo/errormodel.py
+++ src/coherentinfo/errormodel.py
@@ -2,33 +2,37 @@
 
 import numpy as np 
 from numpy.typing import NDArray
+import jax
+from jax import Array
+from jax.typing import ArrayLike
 
 from abc import ABC, abstractmethod
 import scipy
+import jax.numpy as jnp 
 
 
 class ErrorModel(ABC):
     """Abstract class for error models."""
 
-    def __init__(self, num_qudits: int, d: int):
+    def __init__(self, num_subsys: int, d: int):
         """ Init method for the abstract class. 
 
         Args:
-            num_qudits: Length of the error vector to be generated
+            num_subsys: Length of the error vector to be generated
             d: Max value of the elements of the vector plus one.
         """
 
-        self.num_qudits = num_qudits
+        self.num_subsys = num_subsys
         self.d = d
     
     @abstractmethod
-    def get_probabilities(self) -> NDArray:
+    def get_probabilities(self) -> ArrayLike:
         """Computes all the probabilities of having the possible single
         qudit errors and stores them in a numpy array."""
         pass
 
     @abstractmethod
-    def generate_random_error(self) -> NDArray:
+    def generate_random_error(self) -> ArrayLike:
         """Generates an error vector according to the corresponding
         probability distribution"""
         pass 
@@ -48,11 +52,82 @@ class ErrorModel(ABC):
 #     def generate_random_error(self) -> NDArray[int]:
 #         return np.zeros([self.num_qudits], dtype=int)
 
+class ErrorModelBernoulli(ErrorModel):
+    """Class for Qubit error model."""
+
+    def __init__(self, num_subsys: int, d: int=2, p_error: float=0.1):
+        super().__init__(num_subsys, d)
+        if d != 2:
+            raise ValueError("d must be equal to 2 for this error model.")
+        self.p_error = p_error
+        self.probs = self.get_probabilities()
+        # The following cumulative sum is used to create from 
+        # our distribution starting from the uniform distribution 
+        # using the Inverse Transform Sampling method
+        self.cdf = np.cumsum(self.probs)
+
+    def get_probabilities(self) -> NDArray:
+        """Computes the probabilities of having or not a single-qubit error
+        
+        Returns:
+            An array with the probabilities of single-qubit error.
+        """
+        
+        return np.array([1 - self.p_error, self.p_error])
+
+    def generate_random_error(self) -> NDArray[int]:
+        """Generates a random error string where each element is distributed 
+        according to a Poisson distribution. 
+        
+        Returns:
+            An array of integers representing the sampled error.
+
+        """
+        uniform_error = np.random.rand(self.num_subsys)
+        error = np.searchsorted(self.cdf, uniform_error, side='left')
+        return error
+    
+class ErrorModelBernoulliJax(ErrorModel):
+    """Class for Qubit error model using Jax."""
+
+    def __init__(self, num_subsys: int, d: int=2, p_error: float=0.1):
+        super().__init__(num_subsys, d)
+        if d != 2:
+            raise ValueError("d must be equal to 2 for this error model.")
+        self.p_error = p_error
+        self.probs = self.get_probabilities()
+        # The following cumulative sum is used to create from 
+        # our distribution starting from the uniform distribution 
+        # using the Inverse Transform Sampling method
+        self.cdf = jnp.cumsum(self.probs)
+
+    def get_probabilities(self) -> NDArray:
+        """Computes the probabilities of having or not a single-qubit error
+        
+        Returns:
+            An array with the probabilities of single-qubit error.
+        """
+        
+        return jnp.array([1 - self.p_error, self.p_error])
+    
+    def generate_random_error(self, key) -> Array:
+        """Generates a random error string where each element is distributed 
+        according to a Bernoulli distribution.
+        
+        Returns:
+            An array of integers representing the sampled errors.
+
+        """
+        uniform_error = jax.random.uniform(key, self.num_subsys)
+        error = jnp.searchsorted(self.cdf, uniform_error, side='left')
+        return error
+
+
 class ErrorModelPoisson(ErrorModel):
     """Class for Poissonian error model."""
 
-    def __init__(self, num_qudits: int, d: int, gamma_t: float):
-        super().__init__(num_qudits, d)
+    def __init__(self, num_subsys: int, d: int, gamma_t: float):
+        super().__init__(num_subsys, d)
         self.gamma_t = gamma_t
         self.probs = self.get_probabilities()
         # The following cumulative sum is used to create from 
@@ -81,17 +156,46 @@ class ErrorModelPoisson(ErrorModel):
             An array of integers representing the sampled error.
 
         """
-        error = np.zeros([self.num_qudits], dtype=int)
-        def get_single_error() -> int:
-            u = np.random.rand()
-            outcome_index = np.searchsorted(self.cdf, u, side='left')
-            return outcome_index
-
-        for edge in range(self.num_qudits):
-            single_error = get_single_error()
-            error[edge] = single_error
+        uniform_error = np.random.rand(self.num_subsys)
+        error = np.searchsorted(self.cdf, uniform_error, side='left')
         return error
 
+class ErrorModelPoissonJax(ErrorModel):
+    """Class for Poissonian error model using Jax."""
+
+    def __init__(self, num_subsys: int, d: int, gamma_t: float):
+        super().__init__(num_subsys, d)
+        self.gamma_t = gamma_t
+        self.probs = self.get_probabilities()
+        # The following cumulative sum is used to create from 
+        # our distribution starting from the uniform distribution 
+        # using the Inverse Transform Sampling method
+        self.cdf = jnp.cumsum(self.probs)
+
+    def get_probabilities(self) -> Array:
+        """Computes the probabilities of having the d possible
+        single-qudit errors
+        
+        Returns:
+            An array with the probabilities of single-qudit error.
+        """
+        def prob_dist(n: Array) -> Array:
+            prob = jnp.exp(-2 * self.gamma_t) * (2 * self.gamma_t)**n / \
+                jax.scipy.special.factorial(n)
+            return prob
+        return prob_dist(jnp.arange(self.d))
+
+    def generate_random_error(self, key) -> Array:
+        """Generates a random error string where each element is distributed 
+        according to a Poisson distribution. 
+        
+        Returns:
+            An array of integers representing the sampled error.
+
+        """
+        uniform_error = jax.random.uniform(key, self.num_subsys)
+        error = jnp.searchsorted(self.cdf, uniform_error, side='left')
+        return error
 
 
     
--- tests/test_errormodel.py
+++ tests/test_errormodel.py
@@ -1,9 +1,15 @@
 # Tests for the error model
 
 import pytest
-from coherentinfo.errormodel import ErrorModelPoisson
+from coherentinfo.errormodel import (
+    ErrorModelBernoulli, 
+    ErrorModelBernoulliJax,
+    ErrorModelPoisson, 
+    ErrorModelPoissonJax,
+)
 import numpy as np
 from numpy.typing import NDArray
+import jax
 
 def test_instantiate_errormodel():
     """Tests that we can instantiate an error model"""
@@ -13,9 +19,9 @@ def test_instantiate_errormodel():
     except Exception as e:
         pytest.fail(f"Error model instantiation failed with exception: {e}")
 
-def test_inverse_sampling():
-    """Tests that the inverse sampling method gives the correct
-    distribution. """
+def test_inverse_sampling_poisson():
+    """Tests that the inverse sampling method for Poisson 
+    gives the correct distribution. """
 
     num_samples = int(1e6)
     d = 6
@@ -27,6 +33,72 @@ def test_inverse_sampling():
     sampled_frequencies = np.array([
         np.argwhere(error_string == x).shape[0] / num_samples
         for x in range(d)
+    ])
+
+    epsilon = 1e-3 
+    delta = np.sqrt(np.sum((probs - sampled_frequencies)**2))
+    assert delta < epsilon, \
+        f"Sampled probabilities do not match the expected ones."
+    
+def test_inverse_sampling_poisson_jax():
+    """Tests that the inverse sampling method for Poisson with JAX
+    gives the correct distribution. """
+
+    num_samples = int(1e6)
+    d = 6
+    em_poisson = ErrorModelPoissonJax(num_samples, 6, 0.01)
+
+    probs = em_poisson.get_probabilities()
+    seed = 90
+    key = jax.random.PRNGKey(seed)
+    error_string = em_poisson.generate_random_error(key)
+
+    sampled_frequencies = np.array([
+        np.argwhere(error_string == x).shape[0] / num_samples
+        for x in range(d)
+    ])
+
+    epsilon = 1e-3 
+    delta = np.sqrt(np.sum((probs - sampled_frequencies)**2))
+    assert delta < epsilon, \
+        f"Sampled probabilities do not match the expected ones."
+    
+def test_inverse_sampling_bernoulli():
+    """Tests that the inverse sampling method for Bernoulli gives 
+    the correct distribution. """
+
+    num_samples = int(1e6)
+    d = 2
+    em_bernoulli = ErrorModelPoisson(num_samples, 2, 0.01)
+
+    probs = em_bernoulli.get_probabilities()
+    error_string = em_bernoulli.generate_random_error()
+
+    sampled_frequencies = np.array([
+        np.argwhere(error_string == x).shape[0] / num_samples
+        for x in range(d)
+    ])
+
+    epsilon = 1e-3 
+    delta = np.sqrt(np.sum((probs - sampled_frequencies)**2))
+    assert delta < epsilon, \
+        f"Sampled probabilities do not match the expected ones."
+
+def test_inverse_sampling_bernoulli_jax():
+    """Tests that the inverse sampling method for Poisson with JAX
+    gives the correct distribution. """
+
+    num_samples = int(1e6)
+    em_bernoulli_jax = ErrorModelBernoulliJax(num_samples, 2, 0.01)
+
+    probs = em_bernoulli_jax.get_probabilities()
+    seed = 90
+    key = jax.random.PRNGKey(seed)
+    error_string = em_bernoulli_jax.generate_random_error(key)
+
+    sampled_frequencies = np.array([
+        np.argwhere(error_string == x).shape[0] / num_samples
+        for x in range(2)
     ])
 
     epsilon = 1e-3 
